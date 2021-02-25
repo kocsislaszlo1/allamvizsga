6929d18ea7d03fb10376b05ea6a664ac6d4582ea
unknown <kocsislaszlo@uni.sapientia.ro>
2021-02-25 18:17:57 +0200

    example-app

---
+++ example-app/resources/views/dashboard/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('dashboard.layout.main')
+
+@section('title', 'Vezérlőpult')
+
+@section('content')
+
+<div>
+    <p>Vezérlőpult lehetőségek:</p>
+    <ol>
+    	<li>Túrák, rendezvények létehozása, módosítása</li>
+    	<li>Túrabeszámolók, túrafotók feltöltése, módosítása</li>
+    	<li>Felhasználók kezelése: hozzáadás, jogosultságok beállítása</li>
+    </ol>
+</div>
+
+@endsection
